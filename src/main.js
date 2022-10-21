00e905f10b2f00ccc3d56b7cccd9fe56763b6cbf
Eduardo Melo <nascimentomeloedu@gmail.com>
2022-10-21 12:42:13 -0300

    feat: creates regex and masks to validate input fields

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css";
+import IMask from "imask";
 
 const ccBgColor01 = document.querySelector(".cc-bg > svg > g > g:nth-child(1)");
 const ccBgColor02 = document.querySelector(".cc-bg > svg > g > g:nth-child(2)");
@@ -15,7 +16,7 @@ function setCardColorsAndFlag(type) {
       flag: "./cc-mastercard.svg",
     },
     default: {
-      colors: ["black", "#grey}"],
+      colors: ["black", "grey"],
       flag: "./cc-default.svg",
     },
   };
@@ -26,3 +27,59 @@ function setCardColorsAndFlag(type) {
 }
 
 setCardColorsAndFlag("default");
+
+const expirationDateInput = document.querySelector("#expiration-date");
+const currentYear = Number(String(new Date().getFullYear()).slice(2));
+const expirationDateMask = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: currentYear,
+      to: currentYear + 10,
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+  },
+};
+const expirationDateMasked = IMask(expirationDateInput, expirationDateMask);
+
+const securityCodeInput = document.querySelector("#security-code");
+const securityCodeMask = {
+  mask: "000",
+};
+const securityCodeMasked = IMask(securityCodeInput, securityCodeMask);
+
+const cardNumberInput = document.querySelector("#card-number");
+const cardNumberMasks = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      cardType: "default",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardType: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardType: "mastercard",
+    },
+  ],
+  dispatch: function (appendend, dinamicMasked) {
+    const number = (dinamicMasked.value + appendend).replace(/\D/, "");
+
+    const foundMask = dinamicMasked.compiledMasks.find(({ regex }) =>
+      number.match(regex)
+    );
+
+    console.log(foundMask);
+    return foundMask;
+  },
+};
+const cardNumberMasked = IMask(cardNumberInput, cardNumberMasks);
